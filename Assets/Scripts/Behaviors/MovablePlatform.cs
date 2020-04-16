c9082e53b68d1001187b5b9ce8b58dcd19d7f13d
victor <machitavictor@gmail.com>
2020-04-16 14:18:15 +0300

    Move with platform working

--- Assets/Scripts/Behaviors/MovablePlatform.cs
+++ Assets/Scripts/Behaviors/MovablePlatform.cs
@@ -1,34 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
 
 public class MovablePlatform : MonoBehaviour {
-    [SerializeField] private GameObject actualPlatform;
-
     [SerializeField] private GameObject[] intermediaryPoints;
 
     [SerializeField] private float speed = 3;
 
     private int _index;
     private float _maxDistanceBetweenVectors = 1f;
-    void Start() {
-    }
     
-    void Update()
+    void FixedUpdate()
     { 
         if (Vector3.Distance(
-            actualPlatform.transform.position,
+            transform.position,
             intermediaryPoints[_index].transform.position) < _maxDistanceBetweenVectors) {
             _index++;
             if (_index == intermediaryPoints.Length) {
                 _index = 0;
             }   
         } else {
-            actualPlatform.transform.position =
+            transform.position =
                 Vector3.MoveTowards(
-                    actualPlatform.transform.position,
+                    transform.position,
                     intermediaryPoints[_index].transform.position,
                     speed * Time.deltaTime);    
         }
     }
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.CompareTag(Constants.PlayerTag)) {
+            other.transform.parent = transform;    
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.transform.CompareTag(Constants.PlayerTag)) {
+            other.transform.parent = null;   
+        }
+    }
+
 }
+
--- Assets/Utils/Constants.cs
+++ Assets/Utils/Constants.cs
@@ -1,6 +1,10 @@
 ﻿namespace Utils {
     public class Constants {
     
+        //COIN
         public static readonly string CoinTag = "Coin";
+        
+        //PLAYER
+        public static readonly string PlayerTag = "Player";
     }
 }
